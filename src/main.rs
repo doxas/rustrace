5490812022c595c00910ba9e9ebedf64022970f8
doxas <eizanco@gmail.com>
2018-12-11 19:28:07 +0900

    separate logic

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,13 @@ struct Ray {
     direction: Point3D
 }
 
+struct Color {
+    r: u8,
+    g: u8,
+    b: u8,
+    a: u8
+}
+
 fn sphere(p: Point3D, size: f64) -> f64 {
     p.length() - size
 }
@@ -29,45 +36,57 @@ fn normal(p: Point3D) -> Point3D {
     n
 }
 
+fn march(x: u32, y: u32, w: u32, h: u32, count: u32) -> Color {
+    // normalize coordinate
+    let px: f64 = (x as f64 * 2.0 - w as f64) / w as f64;
+    let py: f64 = -(y as f64 * 2.0 - h as f64) / h as f64;
+
+    // initialize variable
+    let mut dest: Color = Color {r: 0, g: 0, b: 0, a: 0};
+    let mut distance: f64 = 0.0;
+    let mut ray: Ray = Ray {
+        position:  Point3D::new(0.0, 0.0, 5.0),
+        direction: Point3D::new(px, py, -1.0)
+    };
+    ray.direction.normalize();
+
+    // ray marching
+    for _ in 0..count {
+        distance = map(ray.position);
+        ray.position.x += ray.direction.x * distance;
+        ray.position.y += ray.direction.y * distance;
+        ray.position.z += ray.direction.z * distance;
+    }
+
+    // hit check
+    if distance < 0.001 {
+        let n: Point3D = normal(ray.position);
+        let mut l: Point3D = Point3D::new(1.0, 1.0, 0.5);
+        l.normalize();
+        // diffuse
+        let diff: f64 = l.dot(n).max(0.1);
+
+        dest.r = (255.0 * diff) as u8;
+        dest.g = (255.0 * diff) as u8;
+        dest.b = (255.0 * diff) as u8;
+    }
+    dest.a = 255;
+    dest
+}
+
 fn main() {
-    let count = 8;
-    let width = 512;
-    let height = 512;
+    let count: u32 = 8;
+    let width: u32 = 512;
+    let height: u32 = 512;
 
     let mut img = image::ImageBuffer::new(width, height);
 
     for (x, y, pixel) in img.enumerate_pixels_mut() {
-        // normalize coordinate
-        let px: f64 = (x as f64 * 2.0 - width as f64) / width as f64;
-        let py: f64 = -(y as f64 * 2.0 - height as f64) / height as f64;
-
-        // initialize variable
-        let mut dest = vec![0, 0, 0, 255];
-        let mut distance: f64 = 0.0;
-        let mut ray: Ray = Ray {
-            position:  Point3D::new(0.0, 0.0, 5.0),
-            direction: Point3D::new(px, py, -1.0)
-        };
-        ray.direction.normalize();
-
-        // ray marching
-        for _ in 0..count {
-            distance = map(ray.position);
-            ray.position.x += ray.direction.x * distance;
-            ray.position.y += ray.direction.y * distance;
-            ray.position.z += ray.direction.z * distance;
-        }
-
-        // hit check
-        if distance < 0.001 {
-            let n: Point3D = normal(ray.position);
-            dest[0] = (n.x.max(0.0) * 255.0) as isize;
-            dest[1] = (n.y.max(0.0) * 255.0) as isize;
-            dest[2] = (n.z.max(0.0) * 255.0) as isize;
-        }
-
         // write color
-        *pixel = image::Rgba([dest[0] as u8, dest[1] as u8, dest[2] as u8, dest[3] as u8]);
+        let dest: Color = march(x, y, width, height, count);
+
+        // return color
+        *pixel = image::Rgba([dest.r, dest.g, dest.b, dest.a]);
     }
 
     img.save("./out/test.png").unwrap();
