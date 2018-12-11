f85309416ac309aa52ab6105d3a790c4f4aeee02
doxas <eizanco@gmail.com>
2018-12-11 14:09:27 +0900

    update source

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,53 @@
 
 extern crate image;
 
-fn main() {
-    let width = 8;
-    let height = 8;
+struct Point {
+    x: f64,
+    y: f64,
+    z: f64
+}
 
-    // construct new imagebuffer
+struct Ray {
+    origin: Point,
+    direction: Point
+}
+
+fn length2d(x: f64, y: f64) -> f64 {
+    let l :f64 = x * x + y * y;
+    l.sqrt()
+}
+
+fn main() {
+    let count = 8;
+    let width = 512;
+    let height = 512;
+
     let mut img = image::ImageBuffer::new(width, height);
 
-    // iterate over all pixels in the image.
     for (x, y, pixel) in img.enumerate_pixels_mut() {
-        let r = x * (255 / width);
-        let g = y * (255 / height);
-        let b = 255;
-        let a = 255;
-        *pixel = image::Rgba([r as u8, g as u8, b as u8, a as u8]);
+
+        // initial
+        // let distance :f64 = 0.0;
+        // let mut ray :Ray = Ray {
+        //     origin: Point {x: 0.0, y: 0.0, z: -5.0},
+        //     direction: Point {x: 0.0, y: 0.0, z: -5.0}
+        // };
+        let mut dest = vec![0, 0, 0, 255];
+
+        let px :f64 = (x as f64 * 2.0 - width as f64) / width as f64;
+        let py :f64 = (y as f64 * 2.0 - height as f64) / height as f64;
+        let pl :f64 = length2d(px, py);
+        let po :f64 = if pl == 0.0 { 1.0 } else { 0.1 / pl };
+        po.min(1.0);
+        po.max(0.0);
+        dest[0] = (po * 255.0) as u8;
+        dest[1] = (po * 255.0) as u8;
+        dest[2] = (po * 255.0) as u8;
+
+        *pixel = image::Rgba([dest[0] as u8, dest[1] as u8, dest[2] as u8, dest[3] as u8]);
     }
 
     img.save("test.png").unwrap();
 }
+
 
