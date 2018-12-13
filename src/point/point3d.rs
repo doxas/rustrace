ceccab3d0b17583c27e124429bdd3b14af61f287
doxas <eizanco@gmail.com>
2018-12-13 14:42:41 +0900

    add testing functions

--- src/point/point3d.rs
+++ src/point/point3d.rs
@@ -1,6 +1,8 @@
 
 use std::ops::Add;
 use std::ops::Sub;
+use std::ops::Mul;
+use std::ops::Div;
 
 #[derive(Copy, Clone)]
 pub struct Point3D {
@@ -22,6 +24,11 @@ impl Point3D {
         self.x /= l;
         self.y /= l;
         self.z /= l;
+    }
+    pub fn scale(&mut self, scalar: f64) {
+        self.x *= scalar;
+        self.y *= scalar;
+        self.z *= scalar;
     }
     pub fn dot(&self, other :Point3D) -> f64 {
         self.x * other.x + self.y * other.y + self.z * other.z
@@ -55,6 +62,44 @@ impl Sub for Point3D {
         }
     }
 }
+impl Mul for Point3D {
+    type Output = Point3D;
+    fn mul(self, other: Point3D) -> Point3D {
+        Point3D {
+            x: self.x * other.x,
+            y: self.y * other.y,
+            z: self.z * other.z
+        }
+    }
+}
+impl Div for Point3D {
+    type Output = Point3D;
+    fn div(self, other: Point3D) -> Point3D {
+        Point3D {
+            x: self.x / other.x,
+            y: self.y / other.y,
+            z: self.z / other.z
+        }
+    }
+}
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_point3d_len() {
+        let p: Point3D = Point3D::new(1.0, 0.0, 0.0);
+        assert_eq!(p.length(), 1.0_f64);
+    }
+
+    #[test]
+    fn test_point3d_normal() {
+        let mut p: Point3D = Point3D::new(10.0, 10.0, 10.0);
+        p.normalize();
+        assert_eq!(p.length(), 1.0_f64);
+        assert!(p.x == p.y && p.y == p.z);
+    }
+}
 
 
