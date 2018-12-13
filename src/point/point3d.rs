2598e9291336297af0e1d6eac2429a1a4f5248a5
doxas <eizanco@gmail.com>
2018-12-13 18:55:58 +0900

    add operator

--- src/point/point3d.rs
+++ src/point/point3d.rs
@@ -4,7 +4,7 @@ use std::ops::Sub;
 use std::ops::Mul;
 use std::ops::Div;
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Point3D {
     pub x: f64,
     pub y: f64,
@@ -15,7 +15,7 @@ impl Point3D {
     pub fn new(x: f64, y: f64, z: f64) -> Point3D {
         Point3D {x: x, y: y, z: z}
     }
-    pub fn length(&self) -> f64 {
+    pub fn length(self) -> f64 {
         let l :f64 = self.x * self.x + self.y * self.y + self.z * self.z;
         l.sqrt()
     }
@@ -25,15 +25,14 @@ impl Point3D {
         self.y /= l;
         self.z /= l;
     }
-    pub fn scale(&mut self, scalar: f64) {
-        self.x *= scalar;
-        self.y *= scalar;
-        self.z *= scalar;
+    pub fn distance(self, other :Point3D) -> f64 {
+        let p: Point3D = other - self;
+        p.length()
     }
-    pub fn dot(&self, other :Point3D) -> f64 {
+    pub fn dot(self, other :Point3D) -> f64 {
         self.x * other.x + self.y * other.y + self.z * other.z
     }
-    pub fn cross(&self, other :Point3D) -> Point3D {
+    pub fn cross(self, other :Point3D) -> Point3D {
         Point3D {
             x: self.y * other.z - self.z * other.y,
             y: self.z * other.x - self.x * other.z,
@@ -92,6 +91,36 @@ impl Add<f64> for Point3D {
         }
     }
 }
+impl Sub<f64> for Point3D {
+    type Output = Point3D;
+    fn sub(self, other: f64) -> Point3D {
+        Point3D {
+            x: self.x - other,
+            y: self.y - other,
+            z: self.z - other
+        }
+    }
+}
+impl Mul<f64> for Point3D {
+    type Output = Point3D;
+    fn mul(self, other: f64) -> Point3D {
+        Point3D {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other
+        }
+    }
+}
+impl Div<f64> for Point3D {
+    type Output = Point3D;
+    fn div(self, other: f64) -> Point3D {
+        Point3D {
+            x: self.x / other,
+            y: self.y / other,
+            z: self.z / other
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -100,30 +129,33 @@ mod tests {
     #[test]
     fn test_point3d_len() {
         let p: Point3D = Point3D::new(1.0, 0.0, 0.0);
-        assert_eq!(p.length(), 1.0_f64);
+        assert_eq!(p.length(), 1.0f64);
     }
-
     #[test]
     fn test_point3d_normal() {
         let mut p: Point3D = Point3D::new(10.0, 10.0, 10.0);
         p.normalize();
-        assert_eq!(p.length(), 1.0_f64);
+        assert_eq!(p.length(), 1.0f64);
         assert!(p.x == p.y && p.y == p.z);
     }
-
+    #[test]
+    fn test_point3d_distance() {
+        let p: Point3D = Point3D::new(10.0, 0.0, 0.0);
+        assert_eq!(p.distance(Point3D::new(0.0, 0.0, 0.0)), 10.0f64);
+    }
     #[test]
     fn test_point3d_add_point3d() {
         let mut p: Point3D = Point3D::new(10.0, 10.0, 10.0);
-        // 自身同士でも大丈夫
         p = p + p;
-        assert_eq!(p.x, 20.0);
+        assert_eq!(p.x, 20.0f64);
+        assert!(p.x == p.y && p.y == p.z);
     }
-
     #[test]
     fn test_point3d_add_f64() {
         let mut p: Point3D = Point3D::new(10.0, 10.0, 10.0);
         p = p + 10.0;
         assert_eq!(p.x, 20.0);
+        assert!(p.x == p.y && p.y == p.z);
     }
 }
 
